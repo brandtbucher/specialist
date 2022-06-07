cd0a3cba4afe00d55ff6c7dd233935cb9a6f0f32
Brandt Bucher <brandtbucher@microsoft.com>
2022-06-07 15:18:23 -0700

    Fix typo

--- test_specialist.py
+++ test_specialist.py
@@ -9,7 +9,7 @@ import specialist
 
 @pytest.mark.parametrize("code", specialist.CODE)
 def test_get_code_for_path(code: types.CodeType) -> None:
-    """Test that the correcct code is returned for a given path."""
+    """Test that the correct code is returned for a given path."""
     path = pathlib.Path(code.co_filename)
     expected = code if path.is_file() else None
     assert specialist.get_code_for_path(path) is expected
