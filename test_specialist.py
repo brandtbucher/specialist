f68c7740318e7f4e36128953c9db9951c0ef7dc9
Brandt Bucher <brandtbucher@microsoft.com>
2022-06-07 15:17:53 -0700

    Run test_get_code_for_path for all code objects

--- test_specialist.py
+++ test_specialist.py
@@ -11,6 +11,5 @@ import specialist
 def test_get_code_for_path(code: types.CodeType) -> None:
     """Test that the correcct code is returned for a given path."""
     path = pathlib.Path(code.co_filename)
-    if not path.is_file():
-        pytest.skip()
-    assert specialist.get_code_for_path(path) is code
+    expected = code if path.is_file() else None
+    assert specialist.get_code_for_path(path) is expected
